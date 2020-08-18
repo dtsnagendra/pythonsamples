806976f87a5326d751de7b6968adff48201f64bc
Swetha <66375006+ss239@users.noreply.github.com>
2020-08-17 23:05:06 -0500

    Add files via upload

---
+++ find_file_name.py
@@ -0,0 +1,25 @@
+try:
+    file_name = input("enter the file name : ")
+    file_name2 = input("enter the file name2 : ")
+    file = open(file_name,"w")
+    file2 = open(file_name2,"w")
+    text = input("enter the statements : ")
+    text2 = input("enter the statements2 : ")
+    file.write(text)
+    file2.write(text2)
+    file.close()
+    file2.close()
+except Exception as msg:
+    print("Exception is",msg)
+    
+    
+def files():
+    try:
+        file_name = input("enter the file name : ")
+        file = open(file_name,"r")
+        file.read()
+    except:
+        print("the file you enter is invalid please enter the valid name ")
+        files()
+
+files()     
